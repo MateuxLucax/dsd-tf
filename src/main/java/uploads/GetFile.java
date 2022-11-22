958b4b5d8fd0150f75926d9be829cb686995b42d
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2022-11-22 20:00:09 -0300

    fix GetFile + make create-user tests

---
+++ src/main/java/uploads/GetFile.java
@@ -0,0 +1,48 @@
+package uploads;
+
+import infra.*;
+import infra.request.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class GetFile extends RequestHandler {
+
+  public GetFile(Request request, SharedContext ctx) {
+    super(request, ctx);
+  }
+
+  private record RequestData(String filename) {};
+
+  public Response run() throws SQLException {
+
+    try {
+      var data = readJson(RequestData.class);
+      var filename = data.filename();
+
+      var file = new File("./uploads/" + filename);
+      if (!file.exists()) {
+        throw new ErrorResponse("badRequest", MsgCode.FILE_DOES_NOT_EXISTS);
+      }
+      if (!file.canRead()) {
+        throw new ErrorResponse("internal", MsgCode.IO_ERROR);
+      }
+
+      var fileContents = new byte[(int) file.length()];
+
+      try (var in = new FileInputStream(file)) {
+        in.read(fileContents);
+      }
+
+      return new Response(true, "", fileContents);
+
+    } catch (ErrorResponse e) {
+      return responseFactory.err(e);
+    } catch (IOException e) {
+      System.err.println(e);
+      return responseFactory.err(new ErrorResponse("internal", MsgCode.IO_ERROR));
+    }
+  }
+}
--- src/test/java/UserTests.java
+++ src/test/java/UserTests.java
@@ -1,5 +1,10 @@
+import infra.request.MessageCodeBody;
+import infra.request.MsgCode;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.sql.Connection;
 
 import static org.junit.Assert.*;
 
@@ -7,6 +12,24 @@ public class UserTests {
 
     @Before
     public void setup() {
+        try {
+            TestUtils.clearTestsDatabase();
+
+            try (
+              var conn = TestUtils.testConnection();
+              var stmt = conn.createStatement()
+            ) {
+                var sql = "INSERT INTO users (username, fullname, password) VALUES ('admin', 'O Administrador', '1234')";
+                stmt.execute(sql);
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+            fail();
+        }
+    }
+
+    @After
+    public void teardown() {
         try {
             TestUtils.clearTestsDatabase();
         } catch (Exception e) {
@@ -24,13 +47,13 @@ public class UserTests {
     private record CreateUserResponse(long id) {}
 
     @Test
-    public void createUserLoginWhoami() {
+    public void canCreateUser() {
         // arrange
         var user = new CreateUserData("test", "123", "Testimus III");
-
-        // act
         TestResponse resp = null;
         CreateUserResponse json = null;
+
+        // act
         try {
             resp = TestUtils.jsonRequest("create-user", user);
             json = resp.json(CreateUserResponse.class);
@@ -41,7 +64,28 @@ public class UserTests {
         // assert
         assertNotNull(resp);
         assertNotNull(json);
-        assertEquals(1, json.id);
+        assertEquals(2, json.id);
+    }
+
+    @Test
+    public void cannotCreateUserWithExistingName() {
+        // arrange
+        var user = new CreateUserData("admin", "9193", "The Admin");
+        TestResponse resp = null;
+        MessageCodeBody json = null;
+
+        // act
+        try {
+            resp = TestUtils.jsonRequest("create-user", user);
+            json = resp.json(MessageCodeBody.class);
+        } catch (Exception e) {
+            fail();
+        }
+
+        // assert
+        assertNotNull(resp);
+        assertNotNull(json);
+        assertEquals(MsgCode.USERNAME_IN_USE.name(), json.messageCode());
     }
 
 }
