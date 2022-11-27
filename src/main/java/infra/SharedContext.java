08eb1e053222361ff1e54f742bf258b413345184
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2022-11-27 17:57:35 -0300

    remove some stuff

--- src/main/java/infra/SharedContext.java
+++ src/main/java/infra/SharedContext.java
@@ -5,14 +5,12 @@ import com.google.gson.GsonBuilder;
 import helpers.RecordTypeAdapterFactory;
 import infra.request.ResponseFactory;
 import infra.session.SessionManager;
-import live.LiveSockets;
 
 public class SharedContext {
 
     private final Gson gson;
     private final SessionManager mgr;
     private final ResponseFactory resFac;
-    private final LiveSockets liveSockets;
 
     public SharedContext() {
         this.gson = new GsonBuilder()
@@ -23,8 +21,6 @@ public class SharedContext {
         this.resFac = new ResponseFactory(gson);
 
         this.mgr = new SessionManager();
-
-        this.liveSockets = new LiveSockets();
     }
 
     public SessionManager sessionManager() {
@@ -33,10 +29,6 @@ public class SharedContext {
 
     public ResponseFactory responseFactory() {
         return this.resFac;
-    }
-
-    public LiveSockets getLiveSockets() {
-        return this.liveSockets;
     }
 
     public Gson gson() {
